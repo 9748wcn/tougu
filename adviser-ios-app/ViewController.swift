8dfbc271a89869aec4a31e112aeca2c6db3a0a1c
cherry <974868578@qq.com>
2019-08-29 11:44:11 +0800

    Initial Commit

---
+++ adviser-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adviser-ios-app
+//
+//  Created by HDCF001 on 2019/8/29.
+//  Copyright © 2019 HDCF001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ adviser-ios-appTests/adviser_ios_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  adviser_ios_appTests.swift
+//  adviser-ios-appTests
+//
+//  Created by HDCF001 on 2019/8/29.
+//  Copyright © 2019 HDCF001. All rights reserved.
+//
+
+import XCTest
+@testable import adviser_ios_app
+
+class adviser_ios_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
